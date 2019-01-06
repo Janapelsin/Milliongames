a79005a00ff6d7919fa9bd8a71e6629c0b4ebe9c
Tewann <tewann@gmail.com>
2019-01-06 12:43:14 +0100

    Walls added

--- src/scenes/scene1.js
+++ src/scenes/scene1.js
@@ -1,13 +1,24 @@
 
 export default class Scene1 extends Phaser.Scene {
+   
     constructor() {
         super({ key: 'Scene1'})
     }
+
     preload() {
-        this.load.image('sky', 'assets/sky.png')
+        this.load.image('bg', '../../assets/bg.png')
+        this.load.image('shortborder', '../../assets/shortborder.png')
+        this.load.image('longborder', '../../assets/longborder.png')
     }
 
     create() {
-        this.image = this.add.image(400, 300, 'sky')
+        this.add.image(400,300,'bg')
+        let walls
+        walls = this.physics.add.staticGroup()
+
+        walls.create(400, 20, 'longborder')
+        walls.create(400, 580, 'longborder')
+        walls.create(20, 300, 'shortborder')
+        walls.create(780, 300, 'shortborder')
     }
 }
